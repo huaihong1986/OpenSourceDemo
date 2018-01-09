a1b30e0157a9e2a6cfe3f5f1386ccb9e2fc3af8f
015164 <huaihong@uec.cn>
2018-01-09 16:17:31 +0800

    Initial commit 如何将自己在github写的android library开源

---
+++ opensourcelibrary/src/main/java/com/example/opensourcelibrary/ToastUtils.java
@@ -0,0 +1,20 @@
+package com.example.opensourcelibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by zxl on 2018/1/9.
+ */
+public class ToastUtils {
+
+    /**
+     * 显示吐司
+     *
+     * @param context
+     * @param str
+     */
+    public static void showToast(Context context, String str) {
+        Toast.makeText(context, str, Toast.LENGTH_LONG).show();
+    }
+}
